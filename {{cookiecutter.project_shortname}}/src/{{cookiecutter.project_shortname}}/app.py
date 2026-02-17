563ed01ad5911103e0b96b3340ebe5404dc1996b
Min-Hsueh Chiu <peter810601@gmail.com>
2026-02-17 14:10:12 -0800

    added example app

--- {{cookiecutter.project_shortname}}/src/{{cookiecutter.project_shortname}}/app.py
+++ {{cookiecutter.project_shortname}}/src/{{cookiecutter.project_shortname}}/app.py
@@ -1,3 +1,6 @@
 from mpships_infra import create_app
+import os
 
-app = create_app(pages_folder="./pages/")
+path_to_pages = os.path.join(os.path.dirname(__file__), "pages")
+
+app = create_app(pages_folder=path_to_pages)
---
+++ {{cookiecutter.project_shortname}}/src/{{cookiecutter.project_shortname}}/pages/{{cookiecutter.project_shortname}}.py
@@ -0,0 +1,60 @@
+import dash
+from dash import html, dcc, callback, Output, Input
+from dash.exceptions import PreventUpdate
+
+from mpships_infra import get_mp_rester
+
+# Register this page at the endpoint: {{cookiecutter.project_shortname}}
+dash.register_page(__name__, path="/{{cookiecutter.project_shortname}}/")
+
+# Use `mpr` for retrieving data from MPRester
+# Example:
+# mpr.materials.summary.search(
+#        chemsys=["Au"], fields=["material_id", "has_props"]
+#    )
+# See more information at: https://docs.materialsproject.org/downloading-data/using-the-api/getting-started
+mpr = get_mp_rester()
+
+# Define your app layout here
+layout = html.Div(
+    [
+        html.H2({{cookiecutter.project_name}}, style={"textAlign": "center"}),
+        html.Button(
+            "Change Color", 
+            id="color-button", 
+            n_clicks=0,
+        ),
+    ],
+    id="project-div",
+    style={
+        "padding": "40px", 
+        "textAlign": "center", 
+        "border": "1px solid #ccc",
+        "transition": "background-color 0.5s ease"
+    }
+)
+
+# Callbacks in Dash Pages use the global @callback decorator
+@callback(
+    Output("project-div", "style"),
+    Input("color-button", "n_clicks"),
+    prevent_initial_call=True
+)
+def update_background(n_clicks):
+    # Base style
+    style = {
+        "padding": "40px", 
+        "textAlign": "center", 
+        "border": "1px solid #ccc",
+        "transition": "background-color 0.5s ease"
+    }
+
+    # Toggle logic
+    if n_clicks % 2 == 1:
+        style["backgroundColor"] = "#3273dc"
+        style["color"] = "white"
+    else:
+        style["backgroundColor"] = "transparent"
+        style["color"] = "black"
+
+    return style

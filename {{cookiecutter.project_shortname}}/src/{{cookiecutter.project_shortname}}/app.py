006e52ae889c4e2da4f88a5e7acefa52371696db
Min-Hsueh Chiu <peter810601@gmail.com>
2026-03-25 17:03:38 -0700

    add pages_folder

--- {{cookiecutter.project_shortname}}/src/{{cookiecutter.project_shortname}}/app.py
+++ {{cookiecutter.project_shortname}}/src/{{cookiecutter.project_shortname}}/app.py
@@ -1,6 +1,11 @@
+"""
+CRITICAL: DO NOT EDIT. Manual changes may cause system instability.
+"""
+
 from mpships_infra import create_app
 import os
 
 url = "/{{cookiecutter.project_shortname}}/"
+path_to_pages = os.path.join(os.path.dirname(__file__), "pages")
 
-app = create_app(url=url)
+app = create_app(url=url, pages_folder=path_to_pages)
